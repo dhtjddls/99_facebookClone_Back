db2a1dc8977414072aca1db2d6ac16d17d988ff6
dhtjddls <dhtjddls12@naver.com>
2023-05-13 17:03:05 +0900

    feat: single image upload 기능 구현

--- controllers/posts.controllers.js
+++ controllers/posts.controllers.js
@@ -1,23 +1,26 @@
 const PostService = require("../services/posts.service");
 
 class PostsController {
-    postService = new PostService();
+  postService = new PostService();
 
-    // 게시물 작성
-    createPost = async (req, res, next) => {
-        const { title, content, createdAt, updatedAt } = req.body;
-        const { userId, nickname } = res.locals.user;
-        const createPostData = await this.postService.createPost(
-            userId,
-            nickname,
-            title,
-            content,
-            createdAt,
-            updatedAt
-        );
+  // 게시물 작성
+  createPost = async (req, res, next) => {
+    const { title, content, createdAt, updatedAt } = req.body;
+    const { img_url } = req;
+    console.log(img_url);
+    const { userId, nickname } = res.locals.user;
+    // img_url을 imgaes table에 저장해주세요!
+    const createPostData = await this.postService.createPost(
+      userId,
+      nickname,
+      title,
+      content,
+      createdAt,
+      updatedAt
+    );
 
-        res.status(201).json({ data: createPostData });
-    };
+    res.status(201).json({ data: createPostData });
+  };
 }
 
-module.exports = PostsController
+module.exports = PostsController;
