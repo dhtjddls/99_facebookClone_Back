05bd503f6f7a948f1c0dc925b7d2154a62389a40
ystar5008@naver.com <ystar5008@naver.com>
2023-05-18 15:45:16 +0900

    feat : 팔로우 레포지토리 주석삭제

--- repositories/follower.repository.js
+++ repositories/follower.repository.js
@@ -22,49 +22,6 @@ class FollowerRepository {
     return [followList, userInfos]
   }
 
-  //  //현재 user_id : 3
-  //   //Follows 모델을 전체조회
-  //   const followList = await this.Follows.findAll({
-  //     //어디서 ? 
-  //     //user_id과 follower_user_id를 동시에 만족하는 데이터
-  //     where: {
-  //       //user_id:3
-  //       user_id: user_id,
-  //       //follower_user_id: 친구추가를 한 사람의
-  //       //user_id가 follwer_user_id가됨
-  //       follower_user_id: {
-  //         [Op.in]: Sequelize.literal(`(SELECT user_id FROM Users)`),
-  //       },
-  //     },
-  //     //테이블을 include
-  //     include: [
-  //       //Users모델의
-  //       {
-  //         model: Users,
-  //         as: "User",
-  //         //follwer_user_id:1
-  //         //user_id,profile_url, name만 참조함
-  //         attribute: ["user_id", "profile_url", "name"],
-  //       },
-  //     ],
-  //   });
-  //   console.log(followList[0].User)
-  //   return followList
-
-  // getFollowerAll = async (user_id) => {//3
-  //   const findAllPostsData = await this.Users.findAll({
-  //     attributes: ["user_id", "name", "profile_url"],
-  //     include: [
-  //       {
-  //         model: Follows,
-  //         where: { follow_user_id: Sequelize.col("Users.user_id") }
-  //       }
-  //     ],
-  //   });
-  //   console.log(findAllPostsData)
-  //   return findAllPostsData
-  // }
-
   findFollower = async (user_id, follower_user_id) => {
     const postFollowData = await this.Follows.findOne({
       where: {
